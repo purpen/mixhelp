2aa54e895cd8340cef7f3f68da65f3842a4ded1e
Gfuqiang <fuqiang6261@163.com>
2018-04-25 19:45:40 +0800

    从新定义表结构

--- mixhelp/models/problem.py
+++ mixhelp/models/problem.py
@@ -2,39 +2,45 @@
 from mixhelp.extensions import db
 
 
-class ProblemType(db.Model):
-    """问题种类表"""
+class Classify(db.Model):
+    """问题分类表"""
 
-    __tablename__ = 'problem_type'
-
-    id = db.Column(db.Integer, primary_key=True)
-    type_name = db.Column(db.String(50), index=True)
-
-
-class Problem_SPU(db.Model):
-    """分组问题表"""
-
-    __tablename__ = 'problem_spu'
+    __tablename__ = 'classify'
 
     id = db.Column(db.Integer, primary_key=True)
-    spu_name = db.Column(db.String(50), index=True)
-    sku_id = db.Column(db.Integer, db.ForeignKey('problem_type.id'))
-    name = db.relationship('Problem_SKU', backref='sku')
+    type_name = db.Column(db.String(50), index=True, unique=True)
+    group = db.relationship('Group')
 
 
-class Problem_SKU(db.Model):
-    """详细问题表"""
+class Group(db.Model):
+    """问题分组表"""
 
-    __tablename__ = 'problem_sku'
+    __tablename__ = 'group'
 
     id = db.Column(db.Integer, primary_key=True)
-    sku_name = db.Column(db.String(50), index=True)
-    title = db.Column(db.String(50))
-    content = db.Column(db.String(200))
+    group_name = db.Column(db.String(50), unique=True, index=True)
+    classify_id = db.Column(db.String(50), db.ForeignKey('classify.type_name'))
+    info = db.relationship('Info')
+
+
+class Info(db.Model):
+    """信息表"""
+
+    __tablename__ = 'info'
+
+    id = db.Column(db.Integer, primary_key=True)
+    details_name = db.Column(db.String(50), unique=True, index=True)
+    details_title = db.Column(db.String(30))
+    content = db.Column(db.String(300))
     comment_num = db.Column(db.Integer)
     solve_num = db.Column(db.Integer)
-    unsolve_num = db.Column(db.Integer)
-    picture_url = db.Column(db.String(50))
+    unsolved_num = db.Column(db.Integer)
+    image_url = db.Column(db.String(50))
     file_url = db.Column(db.String(50))
-    problem_cause = db.Column(db.String(50))
-    spu_id = db.Column(db.Integer, db.ForeignKey('problem_spu.id'))
+    unsolved_cause = db.Column(db.Integer)
+    info_name = db.Column(db.String(50), db.ForeignKey('group.group_name'))
+
+
+
+
+
