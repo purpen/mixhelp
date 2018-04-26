1f6a1ba626fd00fc23fd31e9ffea8f8a7f98f4d4
Gfuqiang <fuqiang6261@163.com>
2018-04-26 19:22:28 +0800

    返回详情数据接口

--- mixhelp/api_1_0/view.py
+++ mixhelp/api_1_0/view.py
@@ -1,1 +1,30 @@
 # -*- coding:utf-8 -*-
+from . import api
+import logging
+from flask import render_template, jsonify
+
+from mixhelp.models.problem import Group, Info
+
+
+@api.route('/issue/list/<name>', methods=['GET'])
+def issue_group(name):
+
+    try:
+        issue_group = Group.query.filter(Group.group_name == name).all()
+    except Exception as e:
+        logging.error(e)
+
+    return render_template('index.thml', issue_group=issue_group)
+
+
+@api.route('/issue/info/<name>', methods=['GET'])
+def issue_info(name):
+
+    try:
+        issue_info = Info.query.filter(Info.content == name).all()
+    except Exception as e:
+        logging.error(e)
+
+    return render_template('index.thml', issue_info=issue_info)
+
+
