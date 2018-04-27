bcd761fe143e6abe3c1b2a37ffda9d4c28fcf69e
Gfuqiang <fuqiang6261@163.com>
2018-04-27 19:52:58 +0800

    添加问题详情信息接口

--- mixhelp/api_1_0/index.py
+++ mixhelp/api_1_0/index.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 from . import api
 import logging
-from flask import render_template, g
+from flask import render_template, g, request, redirect
 
 from mixhelp.models.problem import Classify, Group, Info
 
@@ -18,30 +18,22 @@ def index():
         try:
             group_data = Group.query.filter(Group.classify_id == data.type_name).all()
             g.group_data = group_data
-            break
         except Exception as e:
             logging.error(e)
-    print 2
-    try:
-        info_data = Info.query.filter(g.group_data.group_name == Info.info_name).all()
-        g.info_data = info_data
-    except Exception as e:
-        logging.error(e)
-
-
-    if g.info_data is None:
-        print 1
-    else:
-        for data in g.info_data:
-            print data.details_name
-
+        break
     data = {
         'type': classify_data,
-        # 'group': issue_group,
-        'info': g.info_data
+        'group': g.group_data
     }
 
     return render_template('index.html', datas=data)
+
+
+
+
+
+
+
 
 
 
